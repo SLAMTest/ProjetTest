afc4de479b2ce0555e34156bb2baf1cb19e1fbb9
Jacques buffeteau <jbuffeteau@lerebours.org>
2019-08-27 14:41:55 +0200

    Création de la solution ProjetForum

---
+++ ProjetForum/ProjetForum/Entity/Sujet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetForum.Entity
+{
+    public class Sujet
+    {
+        public int IdSujet { get; set; }
+        public string NomSujet { get; set; }
+    }
+}
