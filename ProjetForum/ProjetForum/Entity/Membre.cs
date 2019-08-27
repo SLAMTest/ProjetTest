a3ade7dfb8c6a559fac5aaf97fc5c8f5e0b9c70e
Jacques buffeteau <jbuffeteau@lerebours.org>
2019-08-27 15:01:41 +0200

    Création classe membre

---
+++ ProjetForum/ProjetForum/Entity/Membre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetForum.Entity
+{
+    public class Membre
+    {
+        public int IdMembre { get; set; }
+        public string Pseudo { get; set; }
+        public string Avatar { get; set; }
+    }
+}
